2d8aad2905fae1f72d77f4f044860c1ac690474e
Max Danielson-Lyons <danielsonlyonsmax@gmail.com>
2016-05-12 19:22:33 -0700

    Init

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+});
+
+http.listen(3000, function() {
+  console.log('Listening to port:  ' + 3000);
+});
